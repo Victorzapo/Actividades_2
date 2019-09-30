8f5f4b20bf8c4b5fe9fb24c18ac0423c15f804f9
Victorzapo <vzapo99@gmail.com>
2019-10-01 01:32:42 +0200

    AA2 lista

--- src/AA1_02-Test/main.cpp
+++ src/AA1_02-Test/main.cpp
@@ -1,61 +1,83 @@
-#include <SDL.h>
+#include <SDL.h>		// Always needs to be included for an SDL app
+#include <SDL_image.h>
+
 #include <exception>
 #include <iostream>
 #include <string>
 
+//Game general information
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+int main(int, char*[])
+{
+	// --- INIT SDL ---
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+		throw "No es pot inicialitzar SDL subsystems";
 
-void Init(SDL_Window* &m_window, SDL_Renderer* &m_renderer) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) throw "No es pot inicialitzar SDL subsystems";
+	// --- WINDOW ---
+	SDL_Window *m_window{ SDL_CreateWindow("SDL...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
+	if (m_window == nullptr)
+		throw "No es pot inicialitzar SDL_Window";
 
-	m_window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if (m_window == nullptr) throw "No es pot inicialitzar SDL_Window";
+	// --- RENDERER ---
+	SDL_Renderer *m_renderer{ SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC) };
+	if (m_renderer == nullptr)
+		throw "No es pot inicialitzar SDL_Renderer";
 
-	m_renderer = SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED);
-	if (m_renderer == nullptr) throw "No es pot inicialitzar SDL_Renderer";
+	//-->SDL_Image 
+	const Uint8 imgFlags{ IMG_INIT_PNG | IMG_INIT_JPG };
+	if (!(IMG_Init(imgFlags) & imgFlags)) throw "Error: SDL_image init";
 
-	SDL_SetRenderDrawColor(m_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-}
+	//-->SDL_TTF
+	//-->SDL_Mix
 
-void Close(SDL_Window* &m_window, SDL_Renderer* &m_renderer) {
+	// --- SPRITES ---
+		//Background
+	SDL_Texture* bgTexture{ IMG_LoadTexture(m_renderer, "../../res/img/bg.jpg") };
+	if (bgTexture == nullptr) throw "Error: bgTexture init";
+	SDL_Rect bgRect{ 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
+	//-->Animated Sprite ---
+
+	// --- TEXT ---
+
+	// --- AUDIO ---
+
+	// --- GAME LOOP ---
+	SDL_Event event;
+	bool isRunning = true;
+	while (isRunning) {
+		// HANDLE EVENTS
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			case SDL_KEYDOWN:
+				if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false;
+				break;
+			default:;
+			}
+		}
+
+		// UPDATE
+
+		// DRAW
+		SDL_RenderClear(m_renderer);
+		//Background
+		SDL_RenderCopy(m_renderer, bgTexture, nullptr, &bgRect);
+		SDL_RenderPresent(m_renderer);
+
+	}
+
+	// --- DESTROY ---
+	SDL_DestroyTexture(bgTexture);
+	IMG_Quit();
 	SDL_DestroyRenderer(m_renderer);
 	SDL_DestroyWindow(m_window);
-	m_window = nullptr;
-	m_renderer = nullptr;
-	SDL_Quit();
-}
 
-int main(int argc, char *argv[])
-{
-	try {
-		SDL_Window* window = nullptr;
-		SDL_Renderer* renderer = nullptr;
-		Init(window, renderer);
-		SDL_Event e;
-		bool quit = false;
-		SDL_Color newColor{ 0,0,0 }, prevColor{ 0,0,0 };
-		float percent = 0.0f;
-		auto lerp = [](float v0, float v1, float t) { return (1 - t)*v0 + t * v1; };
-		while (!quit) {
-			if (!SDL_PollEvent(&e)) {
-				if (e.type == SDL_QUIT) quit = true;
-				if (e.key.keysym.sym == SDLK_ESCAPE) quit = true;
-			}
-			SDL_RenderClear(renderer);
-			(percent > 1.0f) ? (prevColor = newColor, newColor = { Uint8(rand() % 0xFF), Uint8(rand() % 0xFF), Uint8(rand() % 0xFF) }, percent = 0.0f)
-				: percent += .0002f;
-			SDL_SetRenderDrawColor(renderer, lerp(prevColor.r, newColor.r, percent), lerp(prevColor.g, newColor.g, percent), lerp(prevColor.b, newColor.b, percent), 0xFF);
-			SDL_RenderPresent(renderer);
-		}
-		Close(window, renderer);
-	}
-	catch (std::exception e) {
-		std::cout << e.what() << std::endl;
-		auto sdlError = SDL_GetError();
-		if (strlen(sdlError)) std::cout << "SDL Error: " << sdlError << std::endl;
-		std::cin.get();
-	}
+	// --- QUIT ---
+	SDL_Quit();
+
 	return 0;
 }
